4c541acd06f19cf9122bf3dc2eb82c0235e717ef
MoTahoun <eng.m.tahoun@gmail.com>
2024-08-13 00:26:36 +0200

    correct the display in the docker_init bash to select the used display by the user

--- docker_init.sh
+++ docker_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export DISPLAY=:1
+export DISPLAY=$(w -h $USER | awk '$2 ~ /:[0-9.]*/{print $2}')
+
 # export XDG_RUNTIME_DIR=/tmp/runtime-root
 xhost +local:docker
 docker run -it --rm --name lidar_filtering --net=host -e DISPLAY=$DISPLAY -e QT_X11_NO_MITSHM=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}:/workspace lidar_filtering bash -c "roscore & > /dev/null & sleep 2; bash"
