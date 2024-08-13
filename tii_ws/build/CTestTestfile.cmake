0abddb8d7a765556519b277b3c2afb212bf8d10b
MoTahoun <eng.m.tahoun@gmail.com>
2024-08-13 17:33:04 +0200

    Remove the following directories and files from remote repository after adding them to .gitignore: *.bag *.code-workspace tii_ws/build/ tii_ws/devel/ tii_ws/.catkin_tools/ tii_ws/.vscode/

--- tii_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/tii_ws/src
-# Build directory: /workspace/tii_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("os_lidar_filtering")
